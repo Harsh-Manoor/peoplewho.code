5fcb7e37a763435450c3d322b1c3564a02b4f233
Harsh-Manoor <harshmanoor@gmail.com>
2022-01-16 17:46:45 +0530

    init-commit

---
+++ src/components/HeroSection/HeroSection.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import "./HeroSection.css"
+
+const HeroSection = () => {
+    return (
+        <>
+            <section className="hero-section">
+                <img className="heroSectionImage" src="images/herosectionimage.png" alt="heroImage"/>
+                <h2>The Coding Community</h2>
+                <div className="w-node">
+                    <i class="fab fa-whatsapp whatsappIcon"></i>
+                    <p>let's start talking</p>
+                    <i class="fas fa-arrow-right arrowIcon"></i>
+                </div>
+            </section>
+
+            <section className="community-section">
+                <div className="about-community">
+                    <h2>Another coding community?</h2>
+                    <p>Umm... No. We’re The Coding Community.May sound weird, but we’re a bunch of 4-something people who are a cobination of designers, coder and developers, who want to give back to the community. <br/>
+                    Too early? Who cares. We’re not the most experienced - but we’re working on a lot of stuff & want you to be a part of our journey.
+                    </p>
+                </div>
+                <img src="images/community.png" alt="communityImage"/>
+            </section>
+
+            <section className="">
+                <img src="images/rocket.png" alt="rocketImage"/>
+                <div className="">
+                    <h2>What's in it for You?</h2>
+                    
+                </div>
+            </section>
+
+            <section>
+                <div className="">
+                    <h2>Become a mentor...</h2>
+                    <p>If you’re a person who wants to give back to the community, we’ve got something for you.</p>
+                </div>
+                <img src='images/mentor.png' alt="mentorImage"/>
+            </section>
+        </>
+    )
+}
+
+export default HeroSection
