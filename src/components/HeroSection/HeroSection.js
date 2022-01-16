08a5892a537aba194104534ac3b49e1619e4fd9a
Harsh-Manoor <harshmanoor@gmail.com>
2022-01-16 23:00:12 +0530

    parallax animation

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -2,9 +2,17 @@ import React from 'react'
 import "./HeroSection.css"
 
 const HeroSection = () => {
+
+    const parallax = (e) => {
+        let x = (window.innerWidth / 2 - e.pageX) * 0.01;
+        let y = (window.innerHeight / 2 - e.pageY) * 0.01;
+        document.querySelector(".heroSectionImage").style.transform = `translate(${x}px, ${y}px)`;
+
+    }
+
     return (
         <>
-            <section className="hero-section">
+            <section className="hero-section" onMouseMove={parallax}>
                 <img className="heroSectionImage" src="images/herosectionimage.png" alt="heroImage"/>
                 <h2>The Coding Community</h2>
                 <div className="w-node">
@@ -17,8 +25,8 @@ const HeroSection = () => {
             <section className="community-section">
                 <div className="about-community">
                     <h2>Another coding community?</h2>
-                    <p>Umm... No. We’re The Coding Community.May sound weird, but we’re a bunch of 4-something people who are a cobination of designers, coder and developers, who want to give back to the community. <br/>
-                    Too early? Who cares. We’re not the most experienced - but we’re working on a lot of stuff & want you to be a part of our journey.
+                    <p>Umm... No. We're The Coding Community.May sound weird, but we're a bunch of 4-something people who are a cobination of designers, coder and developers, who want to give back to the community. <br/>
+                    Too early? Who cares. We're not the most experienced - but we're working on a lot of stuff & want you to be a part of our journey.
                     </p>
                 </div>
                 <img src="images/community.png" alt="communityImage"/>
